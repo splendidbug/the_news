5e18022e5b806e80e7a6612b26977ae8d559a5ab
Shreyas Agrawal <48771895+splendidbug@users.noreply.github.com>
2023-05-18 03:11:54 -0700

    added css for cards

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,9 @@
 import {HomeCards} from './HomeCards';
 
 const news_cards = [
-    { link: '/link_of_news1', name: 'news1', img: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Official_Photograph_of_Prime_Minister_Narendra_Modi_Potrait.png', description: 'This is sample description of the first news'},
-    { link: '/link_of_news2', name: 'news2', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Rahul_Gandhi.jpg/330px-Rahul_Gandhi.jpg', description: 'This is sample description of the second news'},
-    { link: '/link_of_news3', name: 'news3', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sonia_Gandhi_2019.jpg/330px-Sonia_Gandhi_2019.jpg', description: 'This is sample description of the third news'}
+    { link: '/link_of_news1', name: 'news1', img: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Official_Photograph_of_Prime_Minister_Narendra_Modi_Potrait.png', description: 'This is sample description of the first news', date: '1 Jan', time:"6:00 AM"},
+    { link: '/link_of_news2', name: 'news2', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Rahul_Gandhi.jpg/330px-Rahul_Gandhi.jpg', description: 'This is sample description of the second news', date: '7 May', time:"12:45 PM"},
+    { link: '/link_of_news3', name: 'news3', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sonia_Gandhi_2019.jpg/330px-Sonia_Gandhi_2019.jpg', description: 'This is sample description of the third news', date: '7 March', time:"2:37 AM"}
 ]
 
 export const Home = () => {
--- src/components/pages/Home/HomeCards.js
+++ src/components/pages/Home/HomeCards.js
@@ -1,5 +1,7 @@
+import './homeCards.css'
+
 export const HomeCards = ({news_data}) => {
-    return (
+    /*return (
         <div class="card">
             {news_data.map(news => {
                 return (
@@ -7,9 +9,66 @@ export const HomeCards = ({news_data}) => {
                         <p>{news.name}</p>
                         <img src={news.img}/>
                         <p>{news.description}</p>
+                        <p>{news.date}</p>
+                        <p>{news.time}</p>
                     </div>
                 );
             })}
         </div>
+    )*/
+
+    return(
+        <div> {/*need to make a scrollable container to place the card containers*/}
+            <div class='container'>
+                <div class='column'>
+                    <div class='post-module'>
+                        <div class='thumbnail'>
+                            <div class='date'>
+                            <div class='day'>7</div>
+                            <div class='month'>Mar</div>
+                            </div>
+                            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg'/>
+                        </div>
+                        <div class='post-content'>
+                            <h1 class='title'>City Lights in New York</h1>
+                            <h2 class='sub_title'>The city that never sleeps.</h2>
+                            <p class='description'>New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                            <div class='post-meta'>
+                                <span class='timestamp'>
+                                    <i class='fa fa-clock-o'></i>
+                                    6 mins ago
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class='column'>
+                    <div class='post-module'>
+                        <div class='thumbnail'>
+                            <div class='date'>
+                            <div class='day'>7</div>
+                            <div class='month'>Mar</div>
+                            </div>
+                            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg'/>
+                        </div>
+                        <div class='post-content'>
+                            <h1 class='title'>City Lights in New York</h1>
+                            <h2 class='sub_title'>The city that never sleeps.</h2>
+                            <p class='description'>New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                            <div class='post-meta'>
+                                <span class='timestamp'>
+                                    <i class='fa fa-clock-o'></i>
+                                    6 mins ago
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+
+
     )
-}
+}   
