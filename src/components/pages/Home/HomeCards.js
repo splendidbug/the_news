aaf4e326ff92a860e352651ec0aa02d8fb321bd3
Shreyas Agrawal <48771895+splendidbug@users.noreply.github.com>
2023-05-19 23:41:30 -0700

    cards load dynamically

--- src/components/pages/Home/HomeCards.js
+++ src/components/pages/Home/HomeCards.js
@@ -1,70 +1,42 @@
 import './homeCards.css'
 
+function handleMouseEnter(e){
+    console.log();
+}
+
 export const HomeCards = ({news_data}) => {
-    /*return (
-        <div class="card">
-            {news_data.map(news => {
-                return (
-                    <div class="card_body">
-                        <p>{news.name}</p>
-                        <img src={news.img}/>
-                        <p>{news.description}</p>
-                        <p>{news.date}</p>
-                        <p>{news.time}</p>
-                    </div>
-                );
-            })}
-        </div>
-    )*/
 
     return(
-        <div> {/*need to make a scrollable container to place the card containers*/}
-            <div class='container'>
-                <div class='column'>
-                    <div class='post-module'>
-                        <div class='thumbnail'>
-                            <div class='date'>
-                            <div class='day'>7</div>
-                            <div class='month'>Mar</div>
+        <div> 
+            <div class='container'> {/*need to make this container scrollable*/}
+                {news_data.map(news => {
+                    return (
+                        <div class='column'>
+                            <div class='post-module' onMouseEnter={handleMouseEnter}>
+                                <div class='thumbnail'>
+                                    <div class='date'>
+                                    <div class='day'>{news.day}</div>
+                                    <div class='month'>{news.month}</div>
+                                    </div>
+                                    <img src={news.img}/>
+                                </div>
+                                <div class='post-content'>
+                                    <h1 class='title'>{news.name}</h1>
+                                    <h2 class='sub_title'>{news.name}</h2>
+                                    <p class='description'>{news.description}</p>
+                                    <div class='post-meta'>
+                                        <span class='timestamp'>
+                                            <i class='fa fa-clock-o'></i>
+                                            {news.time}
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
-                            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg'/>
-                        </div>
-                        <div class='post-content'>
-                            <h1 class='title'>City Lights in New York</h1>
-                            <h2 class='sub_title'>The city that never sleeps.</h2>
-                            <p class='description'>New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
-                            <div class='post-meta'>
-                                <span class='timestamp'>
-                                    <i class='fa fa-clock-o'></i>
-                                    6 mins ago
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class='column'>
-                    <div class='post-module'>
-                        <div class='thumbnail'>
-                            <div class='date'>
-                            <div class='day'>7</div>
-                            <div class='month'>Mar</div>
-                            </div>
-                            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg'/>
-                        </div>
-                        <div class='post-content'>
-                            <h1 class='title'>City Lights in New York</h1>
-                            <h2 class='sub_title'>The city that never sleeps.</h2>
-                            <p class='description'>New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
-                            <div class='post-meta'>
-                                <span class='timestamp'>
-                                    <i class='fa fa-clock-o'></i>
-                                    6 mins ago
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        </div>                
+                    );
+                })}  
                 
+
             </div>
             
         </div>
